7cbfc67d89fdaa764006204ef4597a60992696ed
THOMAS <thomaslunardo@gmail.com>
2023-12-13 17:21:03 +0100

    add recepie fixtures and style

--- src/DataFixtures/RecipeFixture.php
+++ src/DataFixtures/RecipeFixture.php
@@ -10,23 +10,23 @@ class RecipeFixture extends Fixture
 {
     public const RECIPES = [
         ['nameRecipe' => 'Cassoulet Toulousin',
-        'prepareTime' => '45',
-        'cookingTime' => '60',
-        'personNumber' => '2',
+        'prepareTime' => 45,
+        'cookingTime' => 60,
+        'personNumber' => 2,
         'picture' => 'cassoulet_1.jpg',
         ],
     ];
     public function load(ObjectManager $manager): void
     {
 
-        foreach (self::RECIPES as $RecipeFixture) {
+        foreach (self::RECIPES as $recipeFixture) {
             $recipe = new Recipe();
-            $recipe->setNameRecipe($RecipeFixture['nameRecipe']);
-            $recipe->setPrepareTime($RecipeFixture['prepareTime']);
-            $recipe->setCookingTime($RecipeFixture['cookingTime']);
-            $recipe->setPersonNumber($RecipeFixture['cookingTime']);
-            $recipe->setPicture($RecipeFixture['picture']);
-            
+            $recipe->setNameRecipe($recipeFixture['nameRecipe']);
+            $recipe->setPrepareTime($recipeFixture['prepareTime']);
+            $recipe->setCookingTime($recipeFixture['cookingTime']);
+            $recipe->setPersonNumber($recipeFixture['cookingTime']);
+            $recipe->setPicture($recipeFixture['picture']);
+
             $manager->persist($recipe);
         }
         $manager->flush();
