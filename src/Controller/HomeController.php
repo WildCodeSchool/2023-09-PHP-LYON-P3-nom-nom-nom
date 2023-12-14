aeef11aa9ed7c0a9504c3d9b74044816e461ab5f
plbonnier <pl_bonnier@yahoo.fr>
2023-12-14 10:38:51 +0100

    link to page in construction

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,4 +13,10 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
+
+    #[Route('/inconstruction', name: 'app_home_inconstruction')]
+    public function inconstruction(): Response
+    {
+        return $this->render('home/inconstruction.html.twig');
+    }
 }
