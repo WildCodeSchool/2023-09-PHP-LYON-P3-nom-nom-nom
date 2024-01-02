10625bfed25006b658ae1847d51d8871c4e76a17
plbonnier <pl_bonnier@yahoo.fr>
2024-01-02 18:51:51 +0100

    modification

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -111,8 +111,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function eraseCredentials(): void
     {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
     }
 
     public function getLastname(): ?string
