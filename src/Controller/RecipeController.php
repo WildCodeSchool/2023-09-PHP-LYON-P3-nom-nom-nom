e3dd43b83ea27d6c2da7c0ea231477f0db1b1d15
THOMAS <thomaslunardo@gmail.com>
2024-01-05 22:42:29 +0100

    In securtiy.yaml/access control, deletion of app_recipe_nem. So addition of the access security inside new recipe method. Did the same for edit recipe

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -15,6 +15,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/recipe')]
 class RecipeController extends AbstractController
@@ -30,6 +31,12 @@ class RecipeController extends AbstractController
     #[Route('/new', name: 'app_recipe_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
+        if (!$this->isGranted('ROLE_CONTRIBUTOR')) {
+            $this->addFlash('danger', 'Connecter vous pour ajouter une recette.');
+
+            return $this->redirectToRoute('app_recipe_index', [], Response::HTTP_SEE_OTHER);
+        }
+
         $number = 0;
         $recipe = new Recipe();
         $form = $this->createForm(RecipeType::class, $recipe);
@@ -72,7 +79,7 @@ class RecipeController extends AbstractController
     #[Route('/{id}/edit', name: 'app_recipe_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Recipe $recipe, EntityManagerInterface $entityManager): Response
     {
-        if ($this->getUser() !== $recipe->getOwner() && !$this->isGranted('ROLE_ADMIN')) {
+        if ($this->getUser() !== $recipe->getOwner() || !$this->isGranted('ROLE_ADMIN')) {
             $this->addFlash('danger', 'Seul l\'auteur de la recette peut la modifier.');
 
             return $this->redirectToRoute('app_recipe_index', [], Response::HTTP_SEE_OTHER);
