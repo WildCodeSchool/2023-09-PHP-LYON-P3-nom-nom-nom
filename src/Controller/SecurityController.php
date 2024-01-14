ee43b7251bc7fa1b33b60e83bb1635ad828e6d7e
plbonnier <pl_bonnier@yahoo.fr>
2024-01-14 16:23:53 +0100

    link and template page favorite, method in security controller

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +15,15 @@ class SecurityController extends AbstractController
     {
         $user = $this->getUser();
         return $this->render('security/profile.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('my-profil/favorites', name: 'app_profile_favorites_recipes')]
+    public function favoritesRecipes(): Response
+    {
+        $user = $this->getUser();
+        return $this->render('security/profile-favorites.html.twig', [
             'user' => $user,
         ]);
     }
