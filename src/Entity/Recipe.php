ce811dd3a1afa58e7b421722066264302eeb141b
plbonnier <pl_bonnier@yahoo.fr>
2024-01-22 22:51:47 +0100

    css ok

--- src/Entity/Recipe.php
+++ src/Entity/Recipe.php
@@ -237,32 +237,6 @@ class Recipe
         return $this->steps;
     }
 
-    // public function getPicture(): ?string
-    // {
-    //     return $this->picture;
-    // }
-
-    // public function setPicture(?string $picture): static
-    // {
-    //     $this->picture = $picture;
-
-    //     return $this;
-    // }
-
-    // public function setPictureFile(File $image = null): Recipe
-    // {
-    //     $this->pictureFile = $image;
-    //     if ($image) {
-    //         $this->updatedAt = new DateTime('now');
-    //     }
-    //     return $this;
-    // }
-
-    // public function getPictureFile(): ?File
-    // {
-    //     return $this->pictureFile;
-    // }
-
     public function addStep(Step $step): static
     {
         if (!$this->steps->contains($step)) {
--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -7,6 +7,7 @@ use App\Entity\Comment;
 // use App\Entity\user;
 // use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -20,8 +21,15 @@ class CommentType extends AbstractType
             ->add('comment', TypeTextType::class, [
                 'label' => 'Votre commentaire'
             ])
-            ->add('note', TypeTextType::class, [
-                'label' => 'Notez la recette (sur 5)'
+            ->add('note', ChoiceType::class, [
+                'choices' => [
+                    'choisissez une note pour cette recette' => '',
+                    '1' => '1',
+                    '2' => '2',
+                    '3' => '3',
+                    '4' => '4',
+                    '5' => '5',
+                ]
             ])
             ->add('Publiez', SubmitType::class)
         ;
