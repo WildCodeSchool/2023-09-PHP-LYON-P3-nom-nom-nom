d4233ce7f195f2737c41cfeea2eb0ecec4b76666
THOMAS <thomaslunardo@gmail.com>
2024-01-23 16:15:14 +0100

    created a blankNameRecipe and dried the code by put all fields inside getRecipeFields

--- tests/Unit/RecipeTest.php
+++ tests/Unit/RecipeTest.php
@@ -7,21 +7,40 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class RecipeTest extends KernelTestCase
 {
+    public function getRecipeFields(): Recipe
+    {
+        return (new Recipe())
+            ->setNameRecipe('Tarte au potimaron')
+            ->setDescription('La tarte au potimarron, c\'est trop bon')
+            ->setCalorie(3000)
+            ->setDate(new \DateTimeImmutable())
+            ->setCookingTime(60)
+            ->setPrepareTime(45)
+            ->setPersonNumber(50);
+    }
     public function testRecipeEntityIsValid(): void
     {
         self::bootKernel();
         $container = static::getContainer();
 
-        $recipe = new Recipe();
-        $recipe->setNameRecipe('Tarte au potimaron');
-        $recipe->setDescription('La tarte au potimarron, c\'est trop bon');
-        $recipe->setCalorie(3000);
-        $recipe->setDate(new \DateTimeImmutable());
-        $recipe->setCookingTime(60);
-        $recipe->setPrepareTime(45);
-        $recipe->setPersonNumber(50);
+        $recipe = $this->getRecipeFields();
+
         $errors = $container->get('validator')->validate($recipe);
 
         $this->assertCount(0, $errors);
+    }
+
+    public function testBlankNameRecipe(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+
+        $recipe = $this->getRecipeFields();
+
+        $recipe->setNameRecipe('');
+
+        $errors = $container->get('validator')->validate($recipe);
+
+        $this->assertCount(1, $errors);
     }
 }
