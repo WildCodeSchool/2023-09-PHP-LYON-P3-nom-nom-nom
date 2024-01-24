cfbfc6d7ba9e892217319fe6ddc382bb71f27c94
THOMAS <thomaslunardo@gmail.com>
2024-01-24 16:58:29 +0100

    In SecurityController, made /my-profil route at the top of the class. Created the profile_edit.html.twig. Addition of the editProfile method into SecurityController.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,9 +7,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/my-profil')]
 class SecurityController extends AbstractController
 {
-    #[Route('/my-profil', name: 'app_profile')]
+    #[Route('/', name: 'app_profile')]
     public function profile(): Response
     {
         $user = $this->getUser();
@@ -18,7 +19,16 @@ class SecurityController extends AbstractController
         ]);
     }
 
-    #[Route('my-profil/favorite', name: 'app_profile_favorite_recipes')]
+    #[Route('/edit', name: 'app_profile_edit')]
+    public function editProfile(): Response
+    {
+        $user = $this->getUser();
+        return $this->render('security/profile_edit.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/favorite', name: 'app_profile_favorite_recipes')]
     public function favoriteRecipes(RecipeRepository $recipeRepository): Response
     {
         $user = $this->getUser();
