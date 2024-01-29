d3286cd62ca0b4430b4c157203c9075d64562ec1
Aude Monnet <aude.monnet711@gmail.com>
2024-01-29 16:22:37 +0100

    finish a lot of debug in registration, recipe view, admin

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -46,7 +46,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 255)]
     #[Assert\NotBlank(message: 'Vous devez entrer votre nom.')]
     #[Assert\Length(
-        min: 3,
+        min: 1,
         max: 20,
         minMessage: 'Votre Nom de famille doit avoir au minimum {{ limit }} caractères.',
         maxMessage: 'Votre Nom de famille ne doit pas dépasser {{ limit }} caractères.',
@@ -56,7 +56,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 255, nullable: true)]
     #[Assert\NotBlank(message: 'Vous devez entrer votre prénom.')]
     #[Assert\Length(
-        min: 3,
+        min: 1,
         max: 20,
         minMessage: 'Votre prénom doit avoir au minimum {{ limit }} caractères.',
         maxMessage: 'Votre prénom de passe ne doit pas dépasser {{ limit }} caractères.',
